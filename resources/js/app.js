64d8f0c39021cab48ad139f677eeca41f5ecacf6
ugawataisei <ugawa.service@gmail.com>
2022-12-31 22:36:47 +0900

    feat: feature/manager * bootstrap, jquery install

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,9 @@ import './bootstrap';
 
 import Alpine from 'alpinejs';
 import focus from '@alpinejs/focus';
+import jQuery from 'jquery';
+
+window.$ = jQuery;
 window.Alpine = Alpine;
 
 Alpine.plugin(focus);
