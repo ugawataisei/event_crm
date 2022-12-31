26111566c10f899d4aa77b23037179493a69ec18
ugawataisei <ugawa.service@gmail.com>
2022-12-31 15:20:40 +0900

    feat: feature/initial * gate setting

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
-// use Illuminate\Support\Facades\Gate;
+use App\Consts\UserConst;
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -21,10 +23,29 @@ class AuthServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(): void
     {
         $this->registerPolicies();
 
-        //
+        /** @var User $user */
+        Gate::define('admin', function ($user) {
+            return $user->role === UserConst::ADMIN_ROLE;
+        });
+
+        Gate::define('manager-higher', function ($user) {
+            if (UserConst::MANAGER_ROLE_LOWER <= $user->role && $user->role <= UserConst::MANAGER_ROLE_UPPER) {
+                return true;
+            } else {
+                return false;
+            }
+        });
+
+        Gate::define('user-higher', function ($user) {
+            if (UserConst::USER_ROLE_LOWER <= $user->role && $user->role <= UserConst::MANAGER_ROLE_LOWER) {
+                return true;
+            } else {
+                return false;
+            }
+        });
     }
 }
